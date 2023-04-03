aba12abb2290ed1612262add17b526534691969c
Muhammad <120216920+komilovmDev@users.noreply.github.com>
2023-04-03 16:18:33 +0500

    Services uz-ru

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -11,8 +11,13 @@ import reactlogo from '../images/reactlogo.svg'
 import django from '../images/django.svg'
 import python from '../images/python.svg'
 import nodejs from '../images/nodejs.svg'
+import { AppContext } from '../context/AppContext'
+import { useContext } from 'react'
 
 export default function Service() {
+
+    const { lauga } = useContext(AppContext)
+
     return (
         <>
             <div className="container">
@@ -22,8 +27,12 @@ export default function Service() {
                         <div className="servicesMain">
                             <div className="servis">
                                 <div className="servisInfo">
-                                    <span className="servisInfo_Title">Website Development</span>
-                                    <p>Productive and engaging web solutions for smarter work and improved customer service</p>
+                                    <span className="servisInfo_Title">{lauga == 'uzb' ? 'Veb-sayt ishlab chiqish' : 'Развитие веб-сайта'}</span>
+                                    <p>{
+                                        lauga == 'uzb' ?
+                                            "Aqlliroq ishlash va mijozlarga xizmat ko'rsatishni yaxshilash uchun samarali va qiziqarli veb-ychimlar"
+                                            : "Эффективные и привлекательные веб-решения для более эффективной работы и улучшения обслуживания клиентов"
+                                    }</p>
                                     <div className="servisInfoBtns">
                                         <button><img src={jslogo} alt="" /></button>
                                         <button><img src={reactlogo} alt="" /></button>
@@ -36,8 +45,8 @@ export default function Service() {
                             </div>
                             <div className="servis">
                                 <div className="servisInfo">
-                                    <span className="servisInfo_Title">Bot Development</span>
-                                    <p>Native, hybrid, and cross-platform mobile apps for consumer-facing and corporate environments.</p>
+                                    <span className="servisInfo_Title">{lauga == 'uzb' ? "Botni ishlab chiqish" : "Разработка бота"}</span>
+                                    <p>{lauga == 'uzb' ? "Biz sizga ishinggizni osonlashtirish uchun sizga bot yaratib beramiz" : "Мы создадим для вас бота, который облегчит вам работу"}</p>
                                     <div className="servisInfoBtns">
                                         <button><img src={python} alt="" /></button>
                                         <button><img src={nodejs} alt="" /></button>
@@ -48,15 +57,15 @@ export default function Service() {
                         </div>
                     </div>
                     <div className="contact">
-                        <span className="contactTitle">Tell us about your project</span>
+                        <span className="contactTitle">{lauga == 'uzb' ? 'loyihangiz haqida bizga xabar bering' : 'Расскажите нам о своем проекте'}</span>
                         <div className="contactMain">
                             <div className="contactMainInfos">
-                                <input type="text" placeholder='First name' />
-                                <input type="text" placeholder='Last name' />
+                                <input type="text" placeholder={lauga == 'uzb' ? "Ism" : "Имя"} />
+                                <input type="text" placeholder={lauga == 'uzb' ? "Familiya" : "Фамилия"} />
                                 <input type="text" placeholder='Email' />
-                                <input type="text" placeholder='Phone' />
+                                <input type="text" placeholder={lauga == 'uzb' ? "Telefon" : "телефон"} />
                                 <select name="" id="">
-                                    <option value="" selected disabled hidden>Budget</option>
+                                    <option value="" selected disabled hidden>{lauga == 'uzb' ? "Byudjet" : "Бюджет"}</option>
                                     <option value="">0$-100$</option>
                                     <option value="">100$-300$</option>
                                     <option value="">300$-700$</option>
@@ -65,14 +74,14 @@ export default function Service() {
                                     <option value="">2000$+</option>
                                 </select>
                                 <select name="" id="">
-                                    <option value="" selected disabled hidden>Where did you hear about us?</option>
+                                    <option value="" selected disabled hidden>{lauga == 'uzb' ? "Biz haqimizda qayerdan eshitdingiz?" : "Как вы узнали о нас?"}</option>
                                     <option value="">Google</option>
                                     <option value="">Instagram</option>
                                     <option value="">Telegram</option>
                                 </select>
                             </div>
-                            <textarea name="" id="" cols="30" rows="10" placeholder='Message'></textarea>
-                            <button>Submit</button>
+                            <textarea name="" id="" cols="30" rows="10" placeholder={lauga == 'uzb' ? "Xabar" : "Сообщение"}></textarea>
+                            <button>{lauga == 'uzb' ? "Yuborish" : "Отправка"}</button>
                         </div>
                     </div>
                 </main>
